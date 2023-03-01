712c64344acc95e5b13a40d41572f1958ede8fab
CheerfulAnt <CheerfulAnt@outlook.com>
2023-03-01 20:33:06 +0100

    Runtime check, config.json load.

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import os
+# import os
 import sys
 import email_reports
 from dotenv import load_dotenv
 import logging
 import traceback
+import json
 
 
-if sys.version_info[0] < 3:
-    raise Exception("Must be using Python 3")
-
+if sys.version_info < (3, 10):
+    raise Exception('Tested only on Python 3.10, comment this if, to try run on another Python version :-)')
 
 load_dotenv()
 logging.basicConfig(filename='sys.log', level=logging.DEBUG, format='%(asctime)s - %(message)s')
 
-try:
-    1/0
-except:
-    logging.exception("Surprise, error occurred when divided by zero.")
-    email_reports.send_email(subject='[ERROR]', message=traceback.format_exc())
+with open('config.json', 'r', encoding="utf-8") as j_file:
+    j_data = json.load(j_file)
+
+
+# try:
+#     1/0
+# except:
+#     logging.exception("Surprise, error occurred when divided by zero.")
+#     email_reports.send_email(subject='[ERROR]', message=traceback.format_exc())
