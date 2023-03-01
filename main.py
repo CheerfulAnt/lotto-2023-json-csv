11d482b04853fe1c91366e7ecde121fb4d27a47f
CheerfulAnt <CheerfulAnt@outlook.com>
2023-03-01 22:12:44 +0100

    Event log

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-# import os
+#!/usr/bin/python3
+# Project Name: lotto-2023
+# Description: I don't know what is this, work in progress :-)
+#              Tested on Linux, Ubuntu 22.04.2
+# Usage: main.py :-)
+# Author: CheerfulAnt@outlook.com
+# Version: 0.1.0
+# Date: 1 March 2023 - 21:00 (GMT+01:00)
+
+import os
 import sys
 import email_reports
 from dotenv import load_dotenv
@@ -6,19 +15,33 @@ import logging
 import traceback
 import json
 
+if __name__ == '__main__':
 
-if sys.version_info < (3, 10):
-    raise Exception('Tested only on Python 3.10, comment this if, to try run on another Python version :-)')
+    if sys.version_info < (3, 10):
+        raise Exception('Tested only on Python 3.10, comment this if, to try run on another Python version :-)')
 
-load_dotenv()
-logging.basicConfig(filename='sys.log', level=logging.DEBUG, format='%(asctime)s - %(message)s')
+    load_dotenv()
 
-with open('config.json', 'r', encoding="utf-8") as j_file:
-    j_data = json.load(j_file)
+    logging.basicConfig(filename=os.getenv('LOG_FILE'), level=logging.DEBUG, format='%(asctime)s - %(message)s')
+
+    def event_log(event='[UNKNOWN]', exception_show=eval(os.getenv('EXCEPTION_SHOW')), eml_error_log=eval(os.getenv('EMAIL_ERROR_LOG'))):
+
+        if event == '[ERROR]'
+            logging.exception("Surprise, error occurred.")
+
+        if exception_show:
+            print('\033[91m' + traceback.format_exc() + '\033[0m')
+        if eml_error_log:
+            email_reports.send_email(subject='[ERROR]', message=traceback.format_exc())
+
+    def operation_log(, eml_operation=eval(os.getenv('EMAIL_OK_LOG'))):
+
+        if eml_operation:
+            email_reports.send_email(subject=, message=traceback.format_exc())
 
 
-# try:
-#     1/0
-# except:
-#     logging.exception("Surprise, error occurred when divided by zero.")
-#     email_reports.send_email(subject='[ERROR]', message=traceback.format_exc())
+    try:
+        with open(os.getenv('CONFIG_JSON_FILE'), 'r', encoding="utf-8") as j_file:
+            j_data = json.load(j_file)
+    except:
+        event_log()
