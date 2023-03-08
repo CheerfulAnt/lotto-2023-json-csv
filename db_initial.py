8cf7d4ff3cc03c78f9d868fe1d325d410cc2e5b9
CheerfulAnt <CheerfulAnt@outlook.com>
2023-03-09 00:26:22 +0100

    Cosmetic changes.

--- db_initial.py
+++ db_initial.py
@@ -17,20 +17,19 @@ print(cfg.db_config)
 
 # cur.execute(sql)
 
-# sql = '''CREATE TABLE IF NOT EXISTS draws(
-#             draws_id    INT         GENERATED ALWAYS AS IDENTITY,
+
+# sql = '''CREATE TABLE IF NOT EXISTS lotto(
+#             draw_id    INT    NOT NULL,
 #             draw_date   DATE        NOT NULL,
-#             leap_year   BOOLEAN     NOT NULL,
-#             draw_date_sum INT       NOT NULL,
-#             birth_number INT        NOT NULL,
-#             master_number INT       NOT NULL,
-#             balls_csv   CHAR(17)    NOT NULL,
+#             draw_time   BOOLEAN     NOT NULL,
 #             ball_1      INT         NOT NULL,
 #             ball_2      INT         NOT NULL,
 #             ball_3      INT         NOT NULL,
 #             ball_4      INT         NOT NULL,
 #             ball_5      INT         NOT NULL,
 #             ball_6      INT         NOT NULL,
+#             special_results integer ARRAY    NOT NULL,
+#             balls_list   integer ARRAY[4]    NOT NULL,
 #             draw_sum    INT         NOT NULL,
 #             draw_avg    INT         NOT NULL,
 #             draw_median INT         NOT NULL,
@@ -39,4 +38,8 @@ print(cfg.db_config)
 #             draw_one_digit    INT   NOT NULL,
 #             draw_two_digit    INT   NOT NULL,
 #             prime_numbers_qty INT   NOT NULL,
+#             leap_year   BOOLEAN     NOT NULL,
+#             draw_date_sum INT       NOT NULL,
+#             birth_number INT        NOT NULL,
+#             master_number INT       NOT NULL,
 #          PRIMARY KEY(draws_id));'''
